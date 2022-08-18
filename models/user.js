5af6749b1f785325b592e04e7499982f6d0b4c3a
Vyacheslav K <i@kvs142.ru>
2022-08-18 15:30:09 +0300

    fix: PR14 - error Добавление пользователя с некорректным url-адресом в поле avatar

--- models/user.js
+++ models/user.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     validate: {
       validator(v) {
-        return /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_.~#?&\/=]*)$/.test(v);
+        return /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/.test(v);
       },
       message: 'Неверный URL.',
     },
